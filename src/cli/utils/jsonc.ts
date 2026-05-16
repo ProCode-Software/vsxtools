9e080c79fc4c49138e0e970237d628cae0be2a6e
ProCode <68168458+ProCode-Software@users.noreply.github.com>
2026-05-16 00:10:10 +0000

    Add JSONC parser

--- src/cli/utils/jsonc.ts
+++ src/cli/utils/jsonc.ts
@@ -1,7 +1,11 @@
-let parseJSONC: (input: string) => any = JSON.parse
-try {
-    parseJSONC = (await import('bun')).JSONC.parse
-} catch {
-    console.warn('JSONC not available')
+let parseJSONC: (input: string) => any = await import('bun')
+    .then(({ JSONC }) => JSONC.parse)
+    .catch(() => parseFallback)
+
+async function parseFallback(input: string): Promise<any> {
+    const { default: stripComments } = await import('../../vendor/jsonc.js')
+    input = stripComments(input, { trailingCommas: true, whitespace: false })
+    return JSON.parse(input)
 }
+
 export { parseJSONC }
