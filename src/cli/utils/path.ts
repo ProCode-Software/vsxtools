f81cef5c8e1ac06e869b005ad506010b3c84979a
ProCode Software <68168458+ProCode-Software@users.noreply.github.com>
2026-06-02 18:24:03 -0500

    Support directory inputs in config

--- src/cli/utils/path.ts
+++ src/cli/utils/path.ts
@@ -1,3 +1,5 @@
+import { readdirSync, statSync } from 'fs'
+import { relative } from 'node:path'
 import { join, parse } from 'path'
 import * as cli from './cli.ts'
 import { type Product } from '#lib/vsxtools/vsxtools.ts'
@@ -5,6 +7,8 @@ import { type Product } from '#lib/vsxtools/vsxtools.ts'
 export type Resolver = (path: string) => string
 
 export const __assetDir = join(import.meta.dirname, '../../assets')
+
+export const shortPath = (p: string) => relative(process.cwd(), p)
 
 /** Returns an iterator that yields the inputs and outputs in the product */
 export function* ioPairs(
@@ -14,29 +18,33 @@ export function* ioPairs(
     if (outputDir && outputFile) {
         cli.error(`Can't have both 'outputDir' and 'outputFile' in product ${name}`)
     }
-    // inputs is a single string
-    if (typeof inputs == 'string') {
-        if (outputFile) return [inputs, outputFile]
-        inputs = [inputs]
-    }
+    if (typeof inputs == 'string') inputs = [inputs] // `inputs` is a single string
+
     for (const [i, file] of inputs.entries()) {
-        const { name } = parse(file)
-        if (outputDir) {
-            yield [file, join(outputDir, name + '.' + outputExt)]
-            continue
-        }
-        if (outputFile) {
-            if (typeof outputFile == 'string') {
-                yield [file, outputFile.replaceAll('[name]', name)]
+        // An input may be a directory
+        const files = statSync(file).isDirectory()
+            ? readdirSync(file).map(f => join(file, f))
+            : [file]
+
+        for (const file of files) {
+            const { name } = parse(file)
+            if (outputDir) {
+                yield [file, join(outputDir, `${name}.${outputExt}`)]
                 continue
             }
-            // outputFile is an array
-            if (outputFile.length != inputs.length) {
-                cli.error(`'inputs' and 'outputFile' must have the same length`)
+            if (outputFile) {
+                if (typeof outputFile == 'string') {
+                    yield [file, outputFile.replaceAll('[name]', name)]
+                    continue
+                }
+                // outputFile is an array
+                if (outputFile.length != inputs.length) {
+                    cli.error(`'inputs' and 'outputFile' must have the same length`)
+                }
+                yield [file, outputFile[i]]
+                continue
             }
-            yield [file, outputFile[i]]
-            continue
+            yield [file, `${name}.${outputExt}`]
         }
-        yield [file, name + '.' + outputExt]
     }
 }
